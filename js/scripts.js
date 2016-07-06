5c4bcd2e6c13ad2eaf8eb990e00c262e3ea4ce7b
Gray Hildreth <grayhildreth@gmail.com>
2016-07-05 20:58:45 -0700

    adds logic to JavaScript file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
+function logic (number {
+   if (number % 15 === 0) {
+    return "pingpong";
+  } else if (number % 5 === 0) {
+    return "pong";
+  } else if (number % 3 === 0) {
+    return "ping";
+  } else {
+    return number;
+  }
+})
+
+
+
 $(document).ready(function(){
   $("#pingpong").submit(function(event) {
     event.preventDefault();
