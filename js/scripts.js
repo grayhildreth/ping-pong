4763d3c8d6bf31a977f36a27fd3de1caa466298e
Gray Hildreth <grayhildreth@gmail.com>
2016-07-12 11:38:19 -0700

    adds image file, and image to the program. Adds addition info to program instructions.

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
     for (var i = 1; i <= num; i++) {
       if (i % 15 === 0) {
-        $("#output").append("<li>"+"Ping-Pong"+"<li>");}
+        $("#output").append("<li>"+"Ping Pong"+"<li>");}
       else if (i % 3 === 0) {
         $("#output").append("<li>"+"Ping"+"<li>");}
       else if (i % 5 === 0) {
