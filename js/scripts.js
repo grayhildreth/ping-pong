f2624a27f5254eb4acace4db33a83eb27026ce23
Gray Hildreth <grayhildreth@gmail.com>
2016-07-11 22:17:53 -0700

    It adds business logic to JS, and refesh button to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
-function logic (number {
-   if (number % 15 === 0) {
-    return "pingpong";
-  } else if (number % 5 === 0) {
-    return "pong";
-  } else if (number % 3 === 0) {
-    return "ping";
-  } else {
-    return number;
-  }
-})
-
-
-
 $(document).ready(function(){
-  $("#pingpong").submit(function(event) {
-    event.preventDefault();
-}
+  $("#pingPong").submit(function(event){
+    var num = $("#number").val()
+
+    for (var i = 1; i <= num; i++) {
+      if (i % 15 === 0) {
+        $("#output").append("<li>"+"Ping-Pong"+"<li>");}
+      else if (i % 3 === 0) {
+        $("#output").append("<li>"+"Ping"+"<li>");}
+      else if (i % 5 === 0) {
+        $("#output").append("<li>"+"Pong"+"<li>");}
+      else{
+        $("#output").append("<li>"+i+"<li>");}
+
+    };
+
+  event.preventDefault();
+
+  });
+});
