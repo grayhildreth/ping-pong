18b4a9d45c5158ba63e34b08eceabb131f740433
Gray Hildreth <grayhildreth@gmail.com>
2016-07-13 22:08:54 -0700

    Separate business and interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
-$(document).ready(function(){
-  $("#pingPong").submit(function(event){
-    var num = $("#number").val()
+var input = function(num) {
+  for (var i = 1; i <= num; i++) {
+    if (i % 15 === 0) {
+      $("#output").append("<li>Ping Pong</li>");
+    } else if ( i % 5 === 0) {
+      $("#output").append("<li>Pong</li>");
+    } else if (i % 3 === 0) {
+      $("#output").append("<li>Ping</li>");
+    } else
+      $("#output").append("<li>" + i + "</li>");
+  }
+};
 
-    for (var i = 1; i <= num; i++) {
-      if (i % 15 === 0) {
-        $("#output").append("<li>"+"Ping Pong"+"<li>");}
-      else if (i % 3 === 0) {
-        $("#output").append("<li>"+"Ping"+"<li>");}
-      else if (i % 5 === 0) {
-        $("#output").append("<li>"+"Pong"+"<li>");}
-      else{
-        $("#output").append("<li>"+i+"<li>");}
+$(document).ready(function() {
+  $("#pingPong").submit(function(event) {
+    event.preventDefault();
+    var num = $("#number").val();
 
-    };
-
-  event.preventDefault();
-
+    input(num);
   });
 });
